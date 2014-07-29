deb4b9611457842a12806260770a089815efe588
Eddie Hedges <eddiehedges@gmail.com>
2014-07-28 23:34:44 -0500

    pushing to master

---
+++ README.md
@@ -0,0 +1,3 @@
+Personal website of Eddie Hedges powered by jekyll and GitHub pages.
+
+For instructions on hosting jekyll powered sites (with plugins) like this one please see [this blog post](http://edhedges.com/blog/2012/07/30/jekyll-with-plugins-hosted-on-github-pages/)
---
+++ js/tooltips.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+  if($(window).width() > 767){
+    var tipTitle = "";
+    var liveTip = $("<div id='current_tooltip'></div>").hide().appendTo("body");
+
+    $('#social_list').bind("mouseover mouseout mousemove", function(event) {
+      var close_link = $(event.target).closest('a');
+      if (!close_link.length) { return; }
+      var hovered_link = close_link[0];
+
+      if (event.type == "mouseover" || event.type == "mousemove") {
+        liveTip.css({
+          top: event.pageY + 12,
+          left: event.pageX + 12
+        });
+      }
+
+      if (event.type == "mouseover") {
+        tipTitle = hovered_link.title;
+        hovered_link.title = "";
+        liveTip.html("<div>" + tipTitle + "</div>")
+        .show();
+      }
+
+      if (event.type == "mouseout") {
+        liveTip.hide();
+        if (tipTitle) {
+          hovered_link.title = tipTitle;
+        }
+      }
+    });
+  }
+});
